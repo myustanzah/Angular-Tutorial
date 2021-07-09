ce6c5c8d40dc3e580ff22fa1840b95d960550a42
myustanzah <muhammadyustanzah@gmail.com>
2021-07-09 19:25:39 +0700

    fecthing-map

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-declare const google: any;
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+
+  ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: 'AIzaSyDzEM2JAB9b_vOxdgkBxSfXNjjx-yLbmdQ',
+    });
+    let map: google.maps.Map;
+    const center: google.maps.LatLngLiteral = { lat: 30, lng: -110 };
+
+    loader.load().then(() => {
+      map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center,
+        zoom: 8,
+      });
+    });
+  }
 }
