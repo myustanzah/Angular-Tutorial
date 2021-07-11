41311d9631c30666c5974c10971c32cebc593692
myustanzah <muhammadyustanzah@gmail.com>
2021-07-12 01:44:50 +0700

    two way binding haven't run yet

--- my-app/src/app/map/map.component.ts
+++ my-app/src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { EventManager } from '@angular/platform-browser';
 import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
@@ -9,12 +10,26 @@ import { Loader } from '@googlemaps/js-api-loader';
 export class MapComponent implements OnInit {
   constructor() {}
 
+  @Input() a!: number;
+  @Input() b!: number;
+
+  changeKordinat(korLat: number, korLng: number) {
+    this.a = korLat;
+    this.b = korLng;
+    return { lat: korLat, lng: korLng };
+  }
   ngOnInit(): void {
     let loader = new Loader({
       apiKey: 'AIzaSyDzEM2JAB9b_vOxdgkBxSfXNjjx-yLbmdQ',
     });
+    let cordinat;
+    if (!this.a && !this.b) {
+      cordinat = { lat: -6.21462, lng: 106.84515 };
+    } else {
+      cordinat = { lat: this.a, lng: this.b };
+    }
     let map: google.maps.Map;
-    const center: google.maps.LatLngLiteral = { lat: -6.21462, lng: 106.84515 };
+    const center: google.maps.LatLngLiteral = cordinat;
 
     loader.load().then(() => {
       map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
